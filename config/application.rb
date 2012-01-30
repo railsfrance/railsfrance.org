17f14a9bbc7b0de838f0a2b3f55d218e6b6befa1
pi3r <pierre.nespo@gmail.com>
2012-01-30 19:37:59 +0100

    Fixes #38

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module RailsFrance
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :confirmation_password]
 
     # Configure asset pipeline
     config.assets.enabled = true
