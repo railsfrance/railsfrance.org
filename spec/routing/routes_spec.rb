8e702ed3cdcb854d5dbd78d85c2d60689c7deea2
slainer68 <slainer68@gmail.com>
2012-02-09 00:42:54 +0100

    Search engines optimized pagination using routing-filter

---
+++ spec/routing/routes_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "pagination" do
+  it "should be Search Engines Optimized" do
+    get("/membres/page/3").should route_to(:controller => "users", :action => "index", :page => 3)
+    get("/jobs/page/5").should route_to(:controller => "jobs", :action => "index", :page => 5)
+  end
+end
