e4f9186ab1dd0931ffb3875060e832b092feaa52
pi3r <pierre.nespo@gmail.com>
2012-02-11 19:38:30 +0100

    Validates presence of postal_code in Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,7 +16,7 @@ class Event < ActiveRecord::Base
   delegate :username, :to => :user, :prefix => true, :allow_nil => true
   delegate :gravatar_url, :to => :user, :prefix => true, :allow_nil => true
 
-  validates_presence_of :city, :street, :user
+  validates_presence_of :city, :street, :postal_code, :user
   validates :title, :presence => true, :length => { :maximum => 100 }
   validates :description, :presence => true, :length => { :maximum => 500 }
   validates_format_of :website, :with => /^(#{URI::regexp(%w(http https))})$/, :allow_blank => true
