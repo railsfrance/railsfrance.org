6a9b7a31a969665d441a52fe8a14850584d92d60
sylvain.abelard+git@gmail.com <sylvain.abelard+github@gmail.com>
2012-11-17 20:24:12 +0100

    Make every job's company more obvious (views and twitter)

--- app/observers/job_observer.rb
+++ app/observers/job_observer.rb
@@ -1,7 +1,8 @@
 class JobObserver < ActiveRecord::Observer
 
   def after_activation(record)
-    Twitter.update I18n.t('twitter.new_job', title: record.title.truncate(60),
-                          url: Rails.application.routes.url_helpers.job_url(record, host: "railsfrance.org"))
+    Twitter.update I18n.t('twitter.new_job',
+      title: record.company.truncate(20)  + ' - ' + record.title.truncate(40),
+      url: Rails.application.routes.url_helpers.job_url(record, host: "railsfrance.org"))
   end
 end
--- spec/observers/job_spec.rb
+++ spec/observers/job_spec.rb
@@ -7,7 +7,7 @@ describe JobObserver do
   describe '#after_activation' do
     it "should call Twitter.update with a well formated message" do
       Twitter.should_receive(:update).with(I18n.t(
-                                                  'twitter.new_job', title: job.title.truncate(60),
+                                                  'twitter.new_job', title: job.company.truncate(20) + ' - ' + job.title.truncate(40),
                                                   url: Rails.application.routes.url_helpers.job_url(job, host: "railsfrance.org")))
       observer.after_activation(job)
     end
