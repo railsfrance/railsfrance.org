7f6118cd1282b9c2cf8f7a717136fd8251698e0d
sylvain.abelard+git@gmail.com <sylvain.abelard+github@gmail.com>
2012-11-18 14:44:31 +0100

    fix the fixing fixes

--- app/observers/job_observer.rb
+++ app/observers/job_observer.rb
@@ -2,7 +2,7 @@ class JobObserver < ActiveRecord::Observer
 
   def after_activation(record)
     Twitter.update I18n.t('twitter.new_job',
-      title: record.company.truncate(20)  + ' - ' + record.title.truncate(40),
+      title: record.to_s.truncate(80),
       url: Rails.application.routes.url_helpers.job_url(record, host: "railsfrance.org"))
   end
 end
