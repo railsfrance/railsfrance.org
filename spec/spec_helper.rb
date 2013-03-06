bf61dfa1c57c324f19052e2512bd89083b4ee406
Pierre Nespo <pierre.nespo@gmail.com>
2013-03-06 14:10:18 +0100

    Add FactoryGirl to rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ require 'fakeweb'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -20,8 +22,10 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
+
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
