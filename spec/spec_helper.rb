880c4e8fda9a166d88957a459ffe210d1da6b9af
Pierre Nespo <pierre.nespo@gmail.com>
2013-03-08 11:25:13 +0100

    Add Devise methods to rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
 
   # == Mock Framework
   #
