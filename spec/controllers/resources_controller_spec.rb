984eaf8935895730edffb3de1abad2c87b1fd79c
Pierre Nespo <pierre.nespo@gmail.com>
2013-03-08 13:47:49 +0100

    Change succes by successful

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -4,7 +4,7 @@ describe ResourcesController do
   describe 'GET index' do
     subject { get(:index) }
 
-    it { expect(subject).to be_succes }
+    it { expect(subject).to be_successful }
     it { expect(subject).to render_template(:index) }
   end
 end
