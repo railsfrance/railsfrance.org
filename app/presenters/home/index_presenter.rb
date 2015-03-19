80c5af145a7ba87f0c6aba39861f9d981902d848
Camille Roux <roux.camille@gmail.com>
2015-03-19 01:37:30 +0100

    Mise à jour de l'URL de Human Coders

--- app/presenters/home/index_presenter.rb
+++ app/presenters/home/index_presenter.rb
@@ -21,7 +21,7 @@ class Home::IndexPresenter
      {img: 'github.png', link: 'https://github.com', title: 'Github'},
      {img: 'groupon.png', link: "http://www.groupon.fr", title: 'Groupon'},
      {img: 'novagile.png', link: "http://novagile.fr", title: 'Novagile'},
-     {img: 'humancoders.png', link: "http://formations.humancoders.com/formations/ruby-on-rails", title: 'Human Coders Formations - Formation Ruby on Rails'},
+     {img: 'humancoders.png', link: "http://www.humancoders.com/formations/ruby-on-rails", title: 'Formation Ruby on Rails'},
      {img: 'letitcast.png', link: "https://letitcast.com", title: 'Letitcast'},
      {img: 'dimelo.png', link: 'http://dimelo.com', title: 'Dimelo'},
      {img: 'linuxfr.png', link: "http://linuxfr.org", title: 'Linuxfr'},
