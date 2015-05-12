25278a5d8d4371295080bfc028b79eb9ece5f0f0
Pierre Nespo <pierre.nespo@gmail.com>
2015-05-12 17:44:31 +0200

    Run app on Heroku

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require File.expand_path('../../lib/dotcloud.rb', __FILE__)
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
